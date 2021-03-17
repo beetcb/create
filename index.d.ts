205434ab2794da032e8609e190abaf25d147517b
be beet <63141491+beetcb@users.noreply.github.com>
2021-03-17 22:37:24 +0800

    feat: complete basic functionality

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,8 @@
 export declare type Bundler = 'parcel' | 'esbuild' | 'webpack';
+export declare type PkgConf = {
+    [key: string]: any;
+};
+export declare type ModuleConf = {
+    key: string;
+    conf: object;
+};
--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
 import { exec } from 'child_process'
-import { existsSync } from 'fs'
+import { existsSync, readFileSync, writeFileSync } from 'fs'
 import { resolve } from 'path'
 
 export type Bundler = 'parcel' | 'esbuild' | 'webpack'
+export type PkgConf = {
+  [key: string]: any
+}
 
+export type ModuleConf = {
+  key: string
+  conf: object
+}
 function execP(
   command: string
 ): Promise<{
@@ -24,11 +31,24 @@ function exist(fileName: string) {
   return existsSync(resolve(fileName)) ? true : false
 }
 
-function prettier(): { key: string; conf: object } {
+function loadConf() {
+  return JSON.parse(readFileSync(resolve('package.json'), 'utf8'))
+}
+
+function prettier(remoteConf?: PkgConf): ModuleConf {
+  if (remoteConf) {
+    if (remoteConf.prettier) {
+      return {
+        key: 'prettier',
+        conf: remoteConf.prettier,
+      }
+    }
+  }
+
   return {
     key: 'prettier',
     conf: {
-      trailingComma: 'es6',
+      trailingComma: 'es5',
       tabWidth: 2,
       semi: false,
       singleQuote: true,
@@ -36,4 +56,49 @@ function prettier(): { key: string; conf: object } {
   }
 }
 
-function bundler(ty: Bundler) {}
+function bundler(ty?: Bundler) {
+  const deps: Array<string> = []
+  switch (ty) {
+    case 'webpack':
+      deps.push('webpack', 'webpack-cli')
+      break
+    case 'esbuild':
+      deps.push('esbuild')
+      break
+    default:
+      deps.push('parcel')
+  }
+  return deps
+}
+
+;(async () => {
+  if (!exist('package.json')) {
+    await execP('npm init -y')
+    const localConf: PkgConf = loadConf()
+    localConf.devDependencies = {}
+    delete localConf.scripts.test
+
+    // Prettier
+    localConf.prettier = prettier().conf
+    localConf.devDependencies.prettier = 'latest'
+
+    // Bundler
+    bundler().forEach((e) => (localConf.devDependencies[e] = 'latest'))
+
+    // TS
+    localConf.devDependencies.typescript = 'latest'
+
+    // Rewrite package.json
+    writeFileSync(resolve('package.json'), JSON.stringify(localConf))
+
+    // Install deps
+    await execP('npm install')
+
+    // Format package.json
+    await execP('npx prettier -w package.json')
+
+    console.log('✨ Done: ready to go!')
+  } else {
+    console.log('📌 Note: package.json exists!')
+  }
+})()
